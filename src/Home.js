78fe3a3ef7b6c940eab379f59d04c5442994334d
Raphael Peuckert <raphael@spdigital.agency>
2023-09-02 14:50:07 +0200

    started working on the header

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,13 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import './styles/Home.css'
 
 export default function Home() {
     return (
         <div>
-            <Header />
+            <section id="home__hero">
+                <Header />
+            </section>
             <h1>This is the Home page</h1>
             <Footer />
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,33 @@
 import { Link } from "react-router-dom";
+import iconCart from "../assets/shared/desktop/icon-cart.svg";
+import logo from "../assets/shared/desktop/logo.svg";
+import '../styles/Header.css'
 
 export default function Header() {
   return (
     <header>
-      <Link to="/">HOME</Link>
-      <Link to="/headphones">HEADPHONES</Link>
-      <Link to="/speakers">SPEAKERS</Link>
-      <Link to="/earphones">EARPHONES</Link>
+      <Link to="/" className="header-logo">
+        <img src={logo} alt="audiophile" />
+      </Link>
+      <nav className="nav">
+        <ul className="nav__list">
+            <li className="nav__item">
+                <Link className="nav__link" to="/">HOME</Link>
+            </li>
+            <li className="nav__item">
+                <Link className="nav__link" to="/headphones">HEADPHONES</Link>
+            </li>
+            <li className="nav__item">
+                <Link className="nav__link" to="/speakers">SPEAKERS</Link>
+            </li>
+            <li className="nav__item">
+                <Link className="nav__link" to="/earphones">EARPHONES</Link>
+            </li>
+        </ul>
+      </nav>
+      <button id="cart-btn">
+        <img src={iconCart} alt="cart" />
+      </button>
     </header>
   );
 }
